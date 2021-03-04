847f143aaf80ba162a1a05dad7810d93af3fc714
Ian Hancock <northroadian@gmail.com>
2021-03-04 16:09:24 -0600

    javascript functions practice

--- js/create.js
+++
@@ -1,7 +0,0 @@
-// Object of protos 
-const bookProtos = {
-    getSummary: function(){
-
-    }
-    getAge
-}
--- spread.js
+++ spread.js
@@ -1,50 +1,13 @@
-// while loops 
-const name = ['jake','richard','steve'];
-//let i =0;
+const ul = document.querySelector('.people');
 
-//while (i<5){
-//i++
-//}
-//console.log('in loop ' , i )
+const people = ['jim','joe','ron','shawn','dave']
 
-let i = 0;
-while(i < names.length){
-    console.log(names[i]);
-    i++
-}
+let html =``
 
+people.forEach(person =>{
+// create Html template for 
+html+= `<li style="color: purple>${person}</li>`
+});
 
-// do while loops 
-// useful in running a code once. runs the code inside a loop 
-// at least once whether it is true or false. 
-
-
-let i = 5 ; 
-
-do{
-    console.log('val of i is:', i);
-    i++; } while(i<5); 
-
-
-    const age = 25 ;
-
-    if(age >20){
-        console.log('you are over 20 ')
-    
-    }
-else{
-    console.log('you are under 20')
-}
-
-// else if statements 
-
-
-const password = 'password123455'
-if(password.length >=12){
-    console.log('that password is mighty strong')
-} else if (password.length >= 8 ){
-    console.log('Hey there this password works')
-} else{
-    console.log ("this password does not work. ")
-}
-
+console.log(html)
+ul.innerHTML = html;
