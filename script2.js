a75ba6ecf50192bb2ec536caf45ba28744953d80
Ian Hancock <northroadian@gmail.com>
2021-05-05 14:53:25 -0500

    making a form

---
+++ script2.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.signup-form')
+// const username= document.querySelector('#username')
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    console.log(username.value)
+    console.log(form.username.value)
+})
+
+
+
