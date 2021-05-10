11e3f39eb0b8a43b42801a045e38d17c9a717131
Ian Hancock <northroadian@gmail.com>
2021-05-10 16:56:47 -0500

    added event listener to username submit works correctly

--- script2.js
+++ script2.js
@@ -1,11 +1,38 @@
 const form = document.querySelector('.signup-form')
 // const username= document.querySelector('#username')
+const feedback = document.querySelector('.feedback')
+const usernamePattern = /^[a-zA-Z]{6,12}$/
 
 form.addEventListener('submit', event =>{
     event.preventDefault();
-    console.log(username.value)
-    console.log(form.username.value)
+   
+   
+
+    //validation
+    const username = form.username.value; 
+
+
+    if(usernamePattern.test(username)){
+        //feedback good info 
+        feedback.textContent ='that username is valid'
+
+    } else{
+        // feedback info 
+        feedback.textContent='username must contain letters only 6-12 charactures long'
+    }
 })
 
+//live feedback 
+ form.username.addEventListener('keyup', event => {
+    console.log(event)
+    if(usernamePattern.test(event.target.value)){
+        form.username.setAttribute('class','success')
+    } else{
+       form.username.setAttribute('class','error')
+    }
+ });
 
 
+
+// this live feed back is so on the event of every key strike up
+// it is constantly checking values. 
