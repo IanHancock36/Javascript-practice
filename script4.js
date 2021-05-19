2a208ab53f58795c63e33fb565c0a5e89d0cfca5
Ian Hancock <northroadian@gmail.com>
2021-05-19 06:16:52 -0500

    map practice

---
+++ script4.js
@@ -0,0 +1,16 @@
+// map cycles through an array and creates a totally new array. 
+
+
+const prices = [20,10,30,25,15,40,80,5]
+
+// const salePrices = prices.map(price => price /2 )
+
+console.log(salePrices)
+
+const products = [
+    {name: 'pop', price: 20},
+    {name: 'beer', price: 64},
+    {name: 'wine', price: 70},
+    {name: 'kombucha', price: 60},
+    {name: 'water', price: 10},
+]; 
