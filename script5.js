101140de66fa55088685855eeea1eedb057baf74
Ian Hancock <northroadian@gmail.com>
2021-05-27 05:31:53 -0500

    moreasynch practice

--- script5.js
+++ script5.js
@@ -3,7 +3,10 @@
     //XMLHTTP will send a request to the browser
 const request = new XMLHttpRequest(); 
 request.addEventListener('readystatechange', ()=>{
-    console.log(request, request.readyState)
+    // 
+    if (request.readyState === 4){
+        console.log(request.responseText)
+    }
 });
 request.open('GET','https://jsonplaceholder.typicode.com/todos/')
 
