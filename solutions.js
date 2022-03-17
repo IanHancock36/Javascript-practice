2730aebf3fa71fc6e95f6e6110462a078c2d101e
Ian Hancock <ianhancock@Ians-MBP.lan1>
2022-03-17 06:51:43 -0500

    nested array

---
+++ solutions.js
@@ -0,0 +1,11 @@
+// nested array loop. 
+
+const arr = [
+    [1, 2], [3, 4], [5, 6]
+  ];
+  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
