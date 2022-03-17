885a54524cb12734bd34b01cafcc142141aeedb7
Ian Hancock <ianhancock@Ians-MBP.lan1>
2022-03-17 07:00:16 -0500

    changes

--- solutions.js
+++ solutions.js
@@ -1,11 +1,30 @@
-// nested array loop. 
+// nested array loop.
 
 const arr = [
-    [1, 2], [3, 4], [5, 6]
-  ];
-  
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+
+// Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+function multiplyAll(arr) {
+  let product = 1;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      console.log(arr[i][j]);
+    for (let w = 0; w < arr[i].length; w++) {
+      console.log(arr[i][w]);
     }
   }
+  return product;
+}
+
+multiplyAll([
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+]);
