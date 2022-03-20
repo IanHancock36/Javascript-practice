bd7f63fee89addbfd0d1232d8bcebb4a1774aae4
IanHancock36 <northroadian@gmail.com>
2022-03-20 08:54:27 -0500

    recursion

---
+++ Recursion.js
@@ -0,0 +1,16 @@
+// Recursive functions 
+ // function that calls itself
+
+ function Cars (){
+     Cars()
+ }
+
+// a recursive function always has a condition to stop calling itself so no infinte loop.
+
+function Cars (){
+    if (condition){
+
+    } else {
+        Cars()
+    }
+}
