91117902b428c0d4df572c151530b25519cf7f83
IanHancock36 <northroadian@gmail.com>
2022-03-20 14:59:47 -0500

    recursrion

--- Recursion.js
+++ Recursion.js
@@ -18,21 +18,32 @@ function Cars (){
 // Countdown recursive function : 
     // this will create an infinte loop 
 
-function countDown(fromNumber){
-    // console.log(fromNumber);
-    countDown(fromNumber-1);
+// function countDown(fromNumber){
+//     // console.log(fromNumber);
+//     countDown(fromNumber-1);
+// }
+// countDown(3)
+
+
+// function countDown(fromNumber) {
+//     console.log(fromNumber);
+
+//     let nextNumber = fromNumber - 1;
+
+//     if (nextNumber > 0) {
+//         countDown(nextNumber);
+//     }
+// }
+// countDown(3);
+
+
+function countDown(num){
+    if (num <= 0){
+        console.log("All Done")
+        return
+    } 
+    console.log(num)
+    num--
 }
-countDown(3)
-
-
-function countDown(fromNumber) {
-    console.log(fromNumber);
-
-    let nextNumber = fromNumber - 1;
-
-    if (nextNumber > 0) {
-        countDown(nextNumber);
-    }
-}
-countDown(3);
-
+countDown(4)
+// the result will be 4, 3 , 2 , 1 
