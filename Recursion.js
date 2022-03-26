2ff1148f5e3bb16809a6c2ce3531e959c904a1a6
IanHancock36 <northroadian@gmail.com>
2022-03-26 05:29:13 -0500

    whole number rando in range

--- Recursion.js
+++ Recursion.js
@@ -150,3 +150,6 @@ function randomWholeNum() {
   // Only change code below this line.
   return Math.floor(Math.random() * 10);
 }
+
+// generating whole number in range 
+Math.floor(Math.random() * (max - min + 1)) + min
---
+++ WholeNumbers.js
@@ -0,0 +1,9 @@
+// generating whole number 
+
+function randomWholeNum() {
+    // Only change code below this line.
+    return Math.floor(Math.random() * 10);
+  }
+  
+  // generating whole number in range 
+  Math.floor(Math.random() * (max - min + 1)) + min
