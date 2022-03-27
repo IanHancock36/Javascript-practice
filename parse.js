3cae4c3c1cf99a32efefc7ca6d34a38efef15ce1
IanHancock36 <northroadian@gmail.com>
2022-03-27 06:19:56 -0500

    range

--- parse.js
+++ parse.js
@@ -12,3 +12,22 @@ function convertToInteger(str) {
         return n < 1 ? [] : [n, ...countdown(n - 1)];
      }
      console.log(countdown(3))
+
+
+     // range of numbers 
+
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+      return [startNum];
+    } else {
+      let numbers = rangeOfNumbers(startNum, endNum - 1);
+      numbers.push(endNum);
+      return numbers;
+    }
+  }
+  console.log(rangeOfNumbers(3,6))
+  
+  // if 6-3 return 3 else
+    // numbers 3 , 6 -1 
+      // this loops through all cases and decrements the numbers down
+        // numnbers.push(endNum) pushes all the numbers in between 3 and 6 
