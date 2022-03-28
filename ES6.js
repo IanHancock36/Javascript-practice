0962083be269a0f1c186cc60fbf3306ba48eef21
IanHancock36 <northroadian@gmail.com>
2022-03-28 05:50:27 -0500

    functions

--- ES6.js
+++ ES6.js
@@ -20,3 +20,9 @@
     return MATH_CONSTANTS.PI;
   }
   const PI = freezeObj();
+
+  // functions 
+  // When there is no function body, and only a return value, arrow function syntax allows you to omit the keyword return as well as the brackets surrounding the code. This helps simplify smaller functions into one-line statements:
+  const magic = () => {
+    return new Date()
+  }
