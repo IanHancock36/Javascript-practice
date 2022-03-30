50aeabfa78a0503d119acfe55529eb7f3425c0a0
IanHancock36 <northroadian@gmail.com>
2022-03-30 06:31:29 -0500

    destructure

--- ES6.js
+++ ES6.js
@@ -56,3 +56,12 @@
     const sum = (...args) => {
       return args.reduce((a, b) => a + b, 0);
       }
+
+// Use Destructuring Assignment to Assign Variables from Objects
+
+      // Destructuring allows you to assign a new variable name when extracting values. You can do this by putting the new name after a colon when assigning the value.
+
+      const user = { name: 'John Doe', age: 34 };
+    //  Here's how you can give new variable names in the assignment:
+      
+      const { name: userName, age: userAge } = user;
