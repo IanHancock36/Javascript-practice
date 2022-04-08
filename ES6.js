8cf8925115afd607ec0ae6285ba2348779902b3b
IanHancock36 <northroadian@gmail.com>
2022-04-08 06:15:42 -0500

    server

--- ES6.js
+++ ES6.js
@@ -169,3 +169,18 @@ const makeMyServerRequest = new Promise((resolve, reject) => {
     reject("Data not recived")
   }
 });
+
+const makeMyMyServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+
+})
+makeMyMyServerRequest.then(result => {
+  console.log(result);
+  })
