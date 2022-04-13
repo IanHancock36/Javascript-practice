3037d1828db197d38abf88a37f8e6186d1e0fed1
IanHancock36 <northroadian@gmail.com>
2022-04-13 05:42:26 -0500

    basic data structures

---
+++ BasicDataStructures.js
@@ -0,0 +1,4 @@
+let yourArray= ["one", false, true, 68, 76] 
+// this is a one dimmensional array 
+console.log(yourArray.length) // length is 5 elements. 
+console.log(yourArray[3]) // index at 3 = 68 
