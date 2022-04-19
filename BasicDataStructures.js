ff5c83a09a591c8b3dce5941b5b4726b7cd9f622
IanHancock36 <northroadian@gmail.com>
2022-04-19 06:08:52 -0500

    index array loop

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -119,3 +119,22 @@ console.log(spreadOut());
 // this first param in the console.log is the array of arr 
   // second is the element you are checking to see if its in there 
     // is coffee there ? no its -1 in the array. 
+
+
+    // Iterate through array 
+    function filteredArray(arr, elem) {
+      let newArr = [];
+   
+    
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(elem) === -1) {
+          //Checks every parameter for the element and if is NOT there continues the code
+          newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
+        }
+      }
+    
+      // change code above this line
+      return newArr;
+    }
+    console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+    console.log(arr)
