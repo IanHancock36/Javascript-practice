2d5073589c547c648f279a983b6af461fab6fdaf
IanHancock36 <northroadian@gmail.com>
2022-04-22 05:50:05 -0500

    nested data

--- NestedArrays.js
+++ NestedArrays.js
@@ -47,3 +47,19 @@ let nestedArray = [
   
   console.log(foods);
 
+// accessing nested data 
+
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // Only change code below this line
+  userActivity.data.online = 45
+  // Only change code above this line
+  
+  console.log(userActivity);
