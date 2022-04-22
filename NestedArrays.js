53e2e335fdc46b07e2b7dbfdabf3740f190e0273
IanHancock36 <northroadian@gmail.com>
2022-04-22 05:48:20 -0500

    key-value-pairs

--- NestedArrays.js
+++ NestedArrays.js
@@ -7,3 +7,28 @@ let nestedArray = [
  nestedArray[2][1][0][0][0]='I like bannans'
 // going into second array index of 1  which is the deepest array [2][1]
   console.log(nestedArray[2][1][0][0][0])
+
+
+  // key values pairs 
+
+  const tekkenCharacter = {
+    player: 'Hwoarang',
+    fightingStyle: 'Tae Kwon Doe',
+    human: true
+  };
+  // this adds the key value pair not in string but in variable form
+  tekkenCharacter.origin = 'South Korea';
+  // if you want the key value pair in string form must use bracket notation
+  tekkenCharacter['hair color'] = 'blue'
+
+//   this will add the pair in string format below 'hair color' :'blue'
+// {
+//     player: 'Hwoarang',
+//     fightingStyle: 'Tae Kwon Doe',
+//     human: true,
+//     origin: 'South Korea',
+//     'hair color': 'blue'
+//   }
+
+  console.log(tekkenCharacter)
+
