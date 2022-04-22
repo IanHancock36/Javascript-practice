032d7aecce33115981e3b2991d2b4dcd0e82856c
IanHancock36 <northroadian@gmail.com>
2022-04-22 05:29:04 -0500

    stuff

--- NestedArrays.js
+++ NestedArrays.js
@@ -4,5 +4,6 @@ let nestedArray = [
     [[['deepest'], ['deepest']],
       [[['deepest-est?']]]]
   ];
+ nestedArray[2][1][0][0][0]='I like bannans'
 // going into second array index of 1  which is the deepest array [2][1]
   console.log(nestedArray[2][1][0][0][0])
