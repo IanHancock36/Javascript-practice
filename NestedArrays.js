8e6489bf24d596c859a71ab1e164d50f565e8cbe
IanHancock36 <northroadian@gmail.com>
2022-04-22 05:48:39 -0500

    key value pair addition

--- NestedArrays.js
+++ NestedArrays.js
@@ -32,3 +32,18 @@ let nestedArray = [
 
   console.log(tekkenCharacter)
 
+
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  foods.bananas = 13
+  foods.grapes = 35
+  foods.strawberries = 27
+  // Only change code above this line
+  
+  console.log(foods);
+
