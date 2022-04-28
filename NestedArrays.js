f1f65b5ac224a07fcddb47a29628d262752e3c75
IanHancock36 <northroadian@gmail.com>
2022-04-28 05:57:17 -0500

    objects

--- NestedArrays.js
+++ NestedArrays.js
@@ -63,3 +63,23 @@ let userActivity = {
   // Only change code above this line
   
   console.log(userActivity);
+
+
+
+  // drilling down in a object
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  function checkInventory(scannedItem) {
+
+   return foods[scannedItem]
+
+  }
+  
+  console.log(checkInventory("apples"));
