35e89c52b3b44bfb5675044c040de94a9f2221e9
IanHancock36 <northroadian@gmail.com>
2022-05-02 05:53:53 -0500

    iteration

--- object.js
+++ object.js
@@ -60,3 +60,32 @@ function isEveryoneHere(dudesObj) {
 //Uses an array with all of the names which should be present in the object.
 //The every method is used to validate all of names used in conjunction with the hasOwnProperty method result in a value of true being returned during each iteration.
 //If at least one name is not found using the hasOwnProperty method, the every method returns false.
+
+
+
+// Iterate Through the Keys of an Object with a for...in Statement
+
+// using for let user in user (sim to a map)
+
+const people = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+function countOnline(peopleObj) {
+  let result = 0;
+  for (let person in peopleObj) 
+  peopleObj[person].online === true ? result++ : false
+  return result;
+}
+
+// you are iterating through all users and checking is single person is online
+  // you have go into peopleObj and then one level down person and check if online strictly === true
+    // if true add to the count of online people if not return false
+      // we want to return the count/result
