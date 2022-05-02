8bb83c49d377cc2047f61fd1c380db6298c19d4d
IanHancock36 <northroadian@gmail.com>
2022-05-02 06:04:12 -0500

    friends array.push"

--- object.js
+++ object.js
@@ -89,3 +89,67 @@ function countOnline(peopleObj) {
   // you have go into peopleObj and then one level down person and check if online strictly === true
     // if true add to the count of online people if not return false
       // we want to return the count/result
+
+
+
+// Returning all the keys in the users object array 
+   let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+
+  return Object.keys(obj);
+
+}
+
+
+// adding to an existing array. 
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA"
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+
+}
+
+console.log(addFriend(user, "Pete"));
+
+// to add the friend we drill down into the object into the friends array 
+  // using userObj.data.friends 
+    // we use array.push method to add a new friend and that is second parameter 
+     // so we can define later calling our addFriend function 
+      // giving flexibility to call friend anythin we want 
+        // we return the userObj or User Object friends array. 
+          // we can now call addFriend in the "user" object and add "Pete" to the end
+            // of the friends array. 
