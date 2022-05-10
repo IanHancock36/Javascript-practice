a845606a7edb674df492c0982620217ad4104c77
IanHancock36 <northroadian@gmail.com>
2022-05-10 06:41:40 -0500

    largest word in string

--- BasicAlgos.js
+++ BasicAlgos.js
@@ -6,7 +6,7 @@ function convertToF(celsius) {
   }
   
   convertToF(30);
-  console.log(convertToF(30))
+//   console.log(convertToF(30))
 
 
   // reversing a sting 
@@ -29,7 +29,7 @@ function factorialize(num) {
   }
   
   factorialize(5);
-  console.log(factorialize(5))
+//   console.log(factorialize(5))
   
   // the num could be a loop decrementing or subtracting 1 at a time and 
   // when num hits 0 stop the loop? 
@@ -38,3 +38,43 @@ function factorialize(num) {
   // this provides a starting point 
   // Its basically saying
   //5*4 = 20  4-1 = 3 3*20 = 60 3-1= 2 2* 60 = 120 2-1 = 1 1 *120 = 120
+
+
+  // sorting longest word in sentence
+   // doing the .split method (" ") with the space creates an array of every word 
+    // compare length by each index side by side a and b 
+        // return word that is longer set param 
+         // a.length or b.length > than other return that word 
+            // create a new array and then the larger length words will be from largest to smallest
+             // index 0 or word[0] should contain the longest number. 
+
+             function findLongestWord(str) {
+                // Step 1. Split the string into an array of strings
+                let strSplit = str.split(' ');
+                // let strSplit = "The quick brown fox jumped over the lazy dog".split(' ');
+                // let strSplit = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"];
+                
+                // Step 2. Sort the elements in the array this switches the position
+                let longestWord = strSplit.sort(function(a, b) { 
+                  return b.length - a.length;
+                });
+                /* Sorting process
+                  a           b            b.length     a.length     var longestWord
+                "The"      "quick"            5            3         ["quick", "The"]
+                "quick"    "brown"            5            5         ["quick", "brown", "The"]  
+                "brown"    "fox"              3            5         ["quick", "brown", "The", "fox"]
+                "fox"      "jumped"           6            3         ["jumped", quick", "brown", "The", "fox"]
+                "jumped"   "over"             4            6         ["jumped", quick", "brown", "over", "The", "fox"]
+                "over"     "the"              3            4         ["jumped", quick", "brown", "over", "The", "fox", "the"]
+                "the"      "lazy"             4            3         ["jumped", quick", "brown", "over", "lazy", "The", "fox", "the"]
+                "lazy"     "dog"              3            4         ["jumped", quick", "brown", "over", "lazy", "The", "fox", "the", "dog"]
+                */
+                
+                // Step 3. Return the length of the first element of the array
+                return longestWord[0].length; // let longestWord = ["jumped", "quick", "brown", "over", "lazy", "The", "fox", "the", "dog"];
+                                              // longestWord[0]="jumped" => jumped".length => 6
+              }
+              
+          findLongestWord("The quick brown fox jumped over the lazy dog")
+ console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+
