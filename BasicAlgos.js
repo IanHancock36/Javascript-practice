3b052a862ef791674f0394603322a75ef9eddfe3
IanHancock36 <northroadian@gmail.com>
2022-05-11 06:43:08 -0500

    array

--- BasicAlgos.js
+++ BasicAlgos.js
@@ -76,5 +76,22 @@ function factorialize(num) {
               }
               
           findLongestWord("The quick brown fox jumped over the lazy dog")
- console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+//  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
 
+// using map method 
+function findLongestWordLength(str) {
+    return Math.max(...str.split(" ").map(word => word.length));
+  }
+  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+
+  //We provide Math.max with the length of each word as argument, and it will simply return the highest of all.
+
+//Let’s analyze everything inside the Math.max parenthesees to understand how we do that.
+
+//str.split(" ") splits the string into an array, taking spaces as separators. It returns this array: [“The”,"quick,“brown”,“fox”,“jumped”,“over”,“the”,“lazy”,“dog”].
+
+//Then, we will make another array, made from the lengths of each element of the str.split(" ") array with map().
+
+//str.split(" ").map(word => word.length) returns [3, 5, 5, 3, 6, 4, 3, 4, 3]
+
+//Finally, we pass the array as argument for the Math.max function with the spread operator ...
---
+++ largestNumSubArray.js
@@ -0,0 +1,11 @@
+// this finds the highest number inside each sub array and returns one array with the largest numbers
+// [5,27,39,1001]
+
+function largestOfFour(mainArray) {
+    return mainArray.map(function (subArray){
+      return subArray.reduce(function (previousLargestNumber, currentLargestNumber) {
+        return (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
+      }, 0);
+    });
+  }
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
