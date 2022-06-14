31407289219f3ddd85fbd73ed89f7eecd0d3c77c
IanHancock36 <northroadian@gmail.com>
2022-06-14 06:30:31 -0500

    704 bianry search

--- BasicAlgos.js
+++ BasicAlgos.js
@@ -114,3 +114,41 @@ function findLongestWordLength(str) {
 confirmEnding("Sweet Deal", "n")
 
 console.log(confirmEnding("Sweet Deal", "n"))
+
+
+
+
+
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+//**** TIP start at the middle of the array because if in the middle of ascending order 9 is not equal
+// we can eliminate the previous numbers and it is faster run time */
+// on every iteration we will cut the input down by half
+
+// you are going to loop until the left and right meet with each other 
+// subtracting the array condenscing down until they meet 
+  // then I belive you do the final check what is bigger than the other 
+      // then you would return -1 if its not the target
+
+const search = function(nums, target) {
+    let left = 0 
+    let right = nums.length -1 
+    while(left <= right){
+      let middle = left + Math.floor((right - left )/2)
+      if(nums[middle] === target){
+        return middle
+      }else if(mid < target){
+        left = middle + 1 
+      }else{
+        right = middle -1 
+      }
+    }
+    return - 1
+}
+
+//Input: nums = [-1,0,3,5,9,12], target = 9
+//Output: 4
+//Explanation: 9 exists in nums and its index is 4
