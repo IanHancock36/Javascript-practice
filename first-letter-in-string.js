4bc27744a6f23cbeec3da09faeb0511d3847f790
IanHancock36 <northroadian@gmail.com>
2022-08-10 07:27:14 -0500

    map

--- first-letter-in-string.js
+++ first-letter-in-string.js
@@ -1,3 +1,8 @@
+// capatalizing every first letter in a string. 
+    // 
+
+
+
 function titleCase(str) {
     return str;
   }
@@ -6,6 +11,27 @@ function titleCase(str) {
 
 
   const coolVariable = "i like turtles"
-  const iLikeTurtles = coolVariable[0].toLocaleUpperCase()
-
+  // this will return only the "F"
+ //******* const iLikeTurtles = coolVariable[0].toLocaleUpperCase() 
+  // if we concatinate + coolVariable.substring(1)
+    // this will give us the full sentence. you are combining 
+    // "I" + "like turtles"
+  const iLikeTurtles=  coolVariable[0].toLocaleUpperCase() + coolVariable.substring(1)
   console.log(iLikeTurtles)
+
+  // Now we must take the string and split them into individual pieces
+const individualWords = coolVariable.split(" ")
+console.log(words)
+// this will result in ["i", "like", "turtles" ]
+  // now we need to iterate over them now they are insisde the array
+    // iterate the index of 0 of every word the first letter
+ individualWords.map((word)=>{
+        return word[0].toLocaleUpperCase() + word.substring(1)
+    }).join(" ")
+
+    // this will iterate over every word in this array and capatalize first letter
+        // then it will concatinate (joining two things) rest of the word with the first word of the sentence
+            // this will return and array we want a sentence
+                // .join provides it to become a sentence. 
+
+  
