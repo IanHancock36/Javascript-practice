3b7facd939c477f12aed4c2ba3ee752321fa18a1
IanHancock36 <northroadian@gmail.com>
2022-08-10 06:42:30 -0500

    practice

---
+++ first-letter-in-string.js
@@ -0,0 +1,11 @@
+function titleCase(str) {
+    return str;
+  }
+  
+  titleCase("I'm a little tea pot");
+
+
+  const coolVariable = "i like turtles"
+  const iLikeTurtles = coolVariable[0].toLocaleUpperCase()
+
+  console.log(iLikeTurtles)
