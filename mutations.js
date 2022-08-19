c13dbcdd4944f30b568c163c9ae13d94c972b845
Ian Hancock <northroadian@gmail.com>
2022-08-19 07:12:33 -0500

    mutation

---
+++ mutations.js
@@ -0,0 +1,16 @@
+// compare the index of 0 to the index of 1 
+// if the index of 1 contains the same letters but not All the letters of index 0 
+// return true else return false. index of 1 hell compared to index of 0 hello should
+// should return true 
+
+
+
+
+
+
+
+function mutation(arr) {
+    return arr;
+  }
+  
+  mutation(["hello", "hey"]);
