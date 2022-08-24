8c2cc1ff5925fe4405a7619a6b56b51c399cd5d9
IanHancock36 <northroadian@gmail.com>
2022-08-24 07:38:29 -0500

    constructor

--- constructors.js
+++ constructors.js
@@ -5,3 +5,15 @@ function Dog() {
   }
   let hound = new Dog()
   
+
+  function Dog(name,color) {
+    this.name = name,
+    this.color = color
+   this.numLegs = 4 
+  
+  
+  }
+  let terrier = new Dog()
+  terrier.name = "steve",
+  terrier.color = "black"
+  
