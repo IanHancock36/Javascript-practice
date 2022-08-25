73e11080e058128912bf74807db934ecc22d0be6
IanHancock36 <northroadian@gmail.com>
2022-08-25 06:58:31 -0500

    constructor

--- constructors.js
+++ constructors.js
@@ -16,4 +16,22 @@ function Dog() {
   let terrier = new Dog()
   terrier.name = "steve",
   terrier.color = "black"
+
+
+
+  let Bird = function(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+
+let crow = new Bird("Alexis", "black");
+
+crow instanceof Bird;
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(7)
+console.log(myHouse instanceof House)
   
