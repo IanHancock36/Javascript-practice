335b1460a34438da67b5772477b3748b8e98a08d
IanHancock36 <northroadian@gmail.com>
2022-09-01 07:00:12 -0500

    prototype

--- constructors.js
+++ constructors.js
@@ -105,3 +105,19 @@ function joinDogFraternity(candidate) {
    return true 
  } return false 
 }
+
+// setting the prototype with an object to write all your additions
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+numLegs: 4, 
+eat: function(){
+  console.log("woof, woof, woof")
+},
+describe: function(){
+   console.log("My name is " + this.name);
+}
+};
