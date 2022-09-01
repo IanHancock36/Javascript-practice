e5e27fba9091d54494215baf5a4c418987b6122c
IanHancock36 <northroadian@gmail.com>
2022-09-01 07:18:29 -0500

    object type

--- constructors.js
+++ constructors.js
@@ -121,3 +121,13 @@ describe: function(){
    console.log("My name is " + this.name);
 }
 };
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+console.log(Object.prototype.isPrototypeOf(Dog.prototype));
