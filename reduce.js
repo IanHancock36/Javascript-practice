ffabeeeb580a7f627d1d82088d64424fb7359a37
Ian Hancock <northroadian@gmail.com>
2022-11-29 14:16:42 -0600

    rows

--- reduce.js
+++ reduce.js
@@ -25,3 +25,51 @@ function countToFive() {
   // starting at 0 will allow all numbers in the length since index starts at 0 length and index are off by 1 
   // also will increase until i is greater than len 
   
+
+// returns 3 rows of zeros
+  function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+  
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+  
+
+
+
+// returns 3 rows of 2 rows each. 
+
+  function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+  
+    for (let i = 0; i < m; i++) {
+       let row = [];
+      // Adds the m-th row into newArray
+  
+      for (let j = 0; j <n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+  
