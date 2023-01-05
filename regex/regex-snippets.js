3f39844c53e74de093b6d8b28957d422bef7877d
Ian Hancock <northroadian@gmail.com>
2023-01-05 07:48:49 -0600

    regex

--- regex/regex-snippets.js
+++ regex/regex-snippets.js
@@ -8,3 +8,10 @@ console.log(result)
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result2 = petRegex.test(petString);
+
+
+// this matches all global instances of a work in the string as well 
+// as is case insensitve 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
