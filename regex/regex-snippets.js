023bc78ae7ed819b200792fbc78f5ab4592b6d00
IanHancock36 <northroadian@gmail.com>
2023-01-15 06:26:43 -0600

    regex

--- regex/regex-snippets.js
+++ regex/regex-snippets.js
@@ -8,3 +8,17 @@ console.log(result)
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result2 = petRegex.test(petString);
+
+// match all lower case and upper case letters in a sentence
+// this will take the range of the alphabet in this particular 
+// sentence and check whether lower or uppercase 
+// globally 
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let resultz = quoteSample.match(alphabetRegex);
+
+//The g and i modifiers have these meanings:
+
+// g = global, match all instances of the pattern in a string, not just one
+// i = case-insensitive (so, for example, /a/i will match the string "a" or "A".
